a350439f45b9c07f69ff235fbc51ae70c98bccc7
ocesar9 <julio.guimaraes@liferay.com>
2023-10-19 12:02:47 -0300

    Annotation e Inference

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function somar(a, b) {
     return a + b;
 }
 console.log(somar(5, 10));
+let produto = "Livro";
+let preco = 200;
+// produto = 300;
+// preco = 300;
+// const carro: {
+//   marca: string;
+//   portas: number;
+// } = {
+//   marca: "Audi",
+//   portas: 5,
+// };
+const carro = {
+    marca: "Audi",
+    portas: 5,
+};
+// carro.marca = 3;
+const barato = preco < 400 ? true : "produto caro";
+console.log(barato);
+function somar2(a, b) {
+    return `${a + b}`;
+}
+console.log(somar2(10, 4));
+const nitendo = {
+    nome: 'Nitendo',
+    preco: '2000'
+};
+function trasnformarPreco(produto) {
+    produto.preco = 'R$' + produto.preco;
+    return produto;
+}
+console.log(trasnformarPreco(nitendo));
--- script.ts
+++ script.ts
@@ -5,3 +5,45 @@ function somar(a: number, b: number) {
 }
 
 console.log(somar(5, 10));
+
+let produto: string = "Livro";
+let preco: number = 200;
+
+// produto = 300;
+// preco = 300;
+
+// const carro: {
+//   marca: string;
+//   portas: number;
+// } = {
+//   marca: "Audi",
+//   portas: 5,
+// };
+
+const carro = {
+  marca: "Audi",
+  portas: 5,
+};
+
+// carro.marca = 3;
+const barato: boolean | string = preco < 400 ? true : "produto caro";
+console.log(barato);
+
+
+function somar2(a: number, b: number): string {
+  return `${a + b}`;
+}
+
+console.log(somar2(10,4));
+
+const nitendo = {
+  nome: 'Nitendo',
+  preco: '2000'
+}
+
+function trasnformarPreco(produto: {nome:string;preco:string}){
+  produto.preco = 'R$' + produto.preco;
+  return produto;
+}
+
+console.log(trasnformarPreco(nitendo));
